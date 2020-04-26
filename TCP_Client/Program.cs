a411972a1ae633ba67cdedbf1ace2c01ec8d7574
Alan.Lin <eric820209@gmail.com>
2020-04-26 18:40:54 +0800

    加入專案檔案。

---
+++ TCP_Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TCP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //檔案
+            FileStream fs = new FileStream(@"C:\Users\Alan\Desktop\test.txt" ,FileMode.Open, FileAccess.Read, FileShare.Read);
+            byte[] bytes = new byte[fs.Length];
+            fs.Read(bytes,0,(int)fs.Length);
+            fs.Close();
+
+            //連線
+            TcpClient tcpClient = new TcpClient("localhost", 5000);
+           var tcpStream=tcpClient.GetStream();
+            tcpStream.Write(bytes,0,bytes.Length);
+        }
+    }
+}
